253197c83b8f5835554cd5cf53f9fb537eef19dc
satwik.k <satwik.kondapalli@assettl.com>
2025-08-19 12:39:07 +0530

    13

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class AuthService {
 
-  private apiUrl = 'http://localhost:3000/';
+  private apiUrl = 'http://localhost:3000';
   constructor(private http: HttpClient) { }
 
   login(credentials: { email: string, password: string }): Observable<any> {
@@ -21,4 +21,9 @@ export class AuthService {
   forgotPassword(data: {email: string, otp: number, password: string}) {
     return this.http.post(`${this.apiUrl}/forgot-password`, data);
   }
+
+  logout() {
+    localStorage.removeItem('token'); // clear session
+  }
+  
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,4 +10,10 @@ import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 })
 export class NavbarComponent {
 
+  constructor(private authService: AuthService, private router: Router) { }
+
+  logout() {
+    localStorage.removeItem('token'); // clear session
+    this.router.navigate(['/login'], { replaceUrl: true });
+  }
 }
